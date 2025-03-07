0904433d9b6849baec0c7ed76cd29af62fe020ee
ajrp73 <areinpei@uax.es>
2025-03-07 11:27:09 +0100

    Initial Files

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static int ejMetEstáticoClaseSuma(int a, int b){
@@ -5,6 +8,10 @@ public class Main {
     }
 
     //Crear un método para calcular y devolver los segundos a partir de los años, meses y días recibidos
+
+    public static long  getSegsFromAñoMesDía(byte años, byte meses, byte días){
+        return (long) (años*365 + meses*30 + días)*24*60*60;
+    }
 
 
     public static void main(String[] args) {
@@ -17,6 +24,11 @@ public class Main {
 
         short a = (short) 32768;
         System.out.println("a: " +  a);
+
+        // Todos los tipos primitivos tienen una wrap class con múltiples funcionalidades
+        short maxShort= Short.MAX_VALUE;
+
+
         int segsAño= días * horas * mins * segs;
         System.out.println("segs Año: " +  segsAño);
 
@@ -24,5 +36,29 @@ public class Main {
 
         num= ejMetEstáticoClaseSuma(10, 2);
         System.out.println("num: " + num);
+        long totSegs= getSegsFromAñoMesDía((byte)127, (byte)127, (byte)127);
+        System.out.println("totSegs: " + totSegs);
+
+        int []arrIntTeclado1 = new int[2];
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<arrIntTeclado1.length; i++){
+            System.out.println("Introduce el entero num. " + i +" ...");
+            arrIntTeclado1[i]= sc.nextInt();
+        }
+
+        System.out.printf("arrIntTeclado: " + Arrays.toString(arrIntTeclado1));
+
+        int []arrIntTeclado2= new int[2];
+        sc = new Scanner(System.in);
+        for(int i=0; i<arrIntTeclado2.length; i++){
+            System.out.println("Introduce el entero num. " + i +" ...");
+            String lectura= sc.next();
+            arrIntTeclado2[i]= Integer.parseInt(lectura); //hacemos uso de la wrap class
+
+        }
+
+        System.out.printf("arrIntTeclado: " + Arrays.toString(arrIntTeclado2));
+
+
     }
 }
